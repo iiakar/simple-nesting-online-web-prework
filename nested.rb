403b577a2ca960b117b55abc5a100b4bc5906b92
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-18 15:31:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,2 +1,9 @@
 def hopper 
-e
+:grace_hopper =
+
+end 
+
+
+describe "#hopper" do 
+    it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
+      expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
